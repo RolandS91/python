624b0cd5271134f65119ed96e2d6e8bf73d8954e
RolandS91 <roland.s_@windowslive.com>
2024-12-05 14:37:50 +0200

    ilus kilpkonn
    Kilpkonn

---
+++ 01.py
@@ -0,0 +1,20 @@
+# 01. ülesanne
+# Sobols 05.12.2024
+
+
+# See impordib kilpkonna mooduli
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-500,300)
+turtle.pendown()
+turtle.forward(200) #fd, pikslites
+turtle.left(120) # Pööramine kraadides
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+
+# Lõpetab kilpkonna, et ei jookseks kokku
+turtle.done()
