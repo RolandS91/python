367058993d4fd6e8822f076f4c4c02d9354044f4
RolandS91 <roland.s_@windowslive.com>
2024-12-05 15:36:14 +0200

    Create 03.py
    ülesanne 3

---
+++ 03.py
@@ -0,0 +1,15 @@
+# 3. ülesanne
+# Sobols 05.12.2024
+
+nimi = "Roland" # sõne, string, str
+vanus = 33 #int, integer, täisarv
+keskmine_hinne = 4.53 # komaarv, float
+
+# Plussiga saan stringid kokku
+print(nimi+", "+str(vanus)+" aastat vana ja keskmine hinne on "+str(keskmine_hinne)) # string
+# komaga saan mitu asja printida
+print(nimi,",",vanus,"aastat vana ja keskmine hinne on",keskmine_hinne) # komadega
+# lause vormindamine
+print(f"{nimi}, {vanus} aastat vana ja keskmine hinne on {keskmine_hinne}!")
+
+
