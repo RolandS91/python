c52cf9a8c57c29e4c289c75912afd502e39cc256
RolandS91 <roland.s_@windowslive.com>
2024-12-05 14:50:16 +0200

    Update 01.py
    Lisatud süda

--- 01.py
+++ 01.py
@@ -5,8 +5,9 @@
 # See impordib kilpkonna mooduli
 import turtle
 
+# Kolmnurk
 turtle.shape("turtle")
-turtle.speed(0)
+turtle.speed(0) # Reguleeri 1-9
 turtle.penup()
 turtle.goto(-500,300)
 turtle.pendown()
@@ -15,6 +16,18 @@ turtle.left(120) # Pööramine kraadides
 turtle.fd(200)
 turtle.left(120)
 turtle.fd(200)
+turtle.left(120)
+
+# Süda
+
+turtle.penup()
+turtle.goto(-200,300)
+turtle.pendown()
+turtle.fd(125)
+turtle.circle(62.5,180)
+turtle.right(90)
+turtle.circle(62.5,180)
+turtle.fd(125)
 
 # Lõpetab kilpkonna, et ei jookseks kokku
 turtle.done()
