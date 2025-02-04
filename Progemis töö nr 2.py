5771bc6b18311c298a81732cf22a972d10554051
RolandS91 <roland.s_@windowslive.com>
2025-02-04 19:31:41 +0200

    Töö nr 2

---
+++ Progemis töö nr 2.py
@@ -0,0 +1,24 @@
+# Prog töö 2
+# Sobols
+# 04.02.2025
+
+# Pangakonto
+
+def pangakonto(algne_saldo, toiming, summa):
+    if toiming == "deposiit":
+        algne_saldo += summa
+    elif toiming == "valjavote":
+        if summa > algne_saldo:
+            return "Viga: Ebapiisav saldo"
+        algne_saldo -= summa
+    else:
+        return "Viga: Tundmatu toiming"
+    
+    return algne_saldo
+
+# Näidis kasutamine
+saldo = 100
+saldo = pangakonto(saldo, "deposiit", 50)
+print(f"Saldo on: {saldo}")
+saldo = pangakonto(saldo, "valjavote", 20)
+print(f"Saldo on: {saldo}")
